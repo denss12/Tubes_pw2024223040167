955ddd1cc0f23161ebca359d960d49d07940abfb
denss12 <145624652+denss12@users.noreply.github.com>
2024-06-13 12:49:48 +0700

    Delete Tubes_pw2024223040167 directory

--- Tubes_pw2024223040167/inc/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-  $conn = mysqli_connect("localhost", "root", "", "sport_news");
-
-  if(mysqli_connect_errno()){
-    echo "Gagal koneksi ke database". mysqli_connect_error();
-    exit();
-  }
-?>
